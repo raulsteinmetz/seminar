aa657f285aa43c227781ab33f27a73a3d946b96c
raulsteinmetz <rsteinmetz@inf.ufsm.br>
2024-05-07 13:39:59 -0300

    saving logs, plotting learning curve code

--- train.py
+++ train.py
@@ -1,10 +1,13 @@
 import yaml
 import argparse
+import os
+import csv
 
 import gym
 import torch
 import torch.nn as nn
 import numpy as np
+from collections import OrderedDict
 
 from util.buffer import ReplayBuffer
 from util.tools import set_all_seeds
@@ -13,7 +16,7 @@ from util.q_net import learn_ddqn_mlp, learn_dqn_mlp, make_q_nets
 configs = {}
 
 def main(configs):
-    set_all_seeds(configs['seed']) # reproducibility
+    set_all_seeds(configs['seed'])
 
     env = gym.make(configs['env'])
 
@@ -24,53 +27,62 @@ def main(configs):
     elif configs['agent'] == 'ddqn':
         learn = learn_ddqn_mlp
         
-    target_q_net.load_state_dict(q_net.state_dict()) # copy net into target_net
+    target_q_net.load_state_dict(q_net.state_dict())
     optimizer = torch.optim.Adam(q_net.parameters(), configs['learning_rate'])
 
     buffer = ReplayBuffer(configs['replay_buffer_capacity'], env.observation_space.shape[0], 1)
 
-    for episode in range(configs['n_episodes']):
-        observation, _ = env.reset()
-        observation = torch.from_numpy(observation)
-        finished = False
-        episode_length = 0
-        cumulative_reward = 0
+    log_folder = f"logs/{configs['agent']}"
+    os.makedirs(log_folder, exist_ok=True)
+    log_file_path = f"{log_folder}/{configs['env']}.csv"
 
-        while not finished:
-            if episode < configs['warm_up_episodes']:
-                action = env.action_space.sample() # random action for filling buffer
-            else:
-               action = q_net(observation.unsqueeze(0)).argmax(axis=-1).squeeze().item() # actual q_network computation
+    with open(log_file_path, 'w', newline='') as csvfile:
+        fieldnames = ['episode', 'reward', 'episode_length']
+        writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
+        writer.writeheader()
 
-            next_observation, reward, terminated, truncated, _ = env.step(action)
+        for episode in range(configs['n_episodes']):
+            observation, _ = env.reset()
+            observation = torch.from_numpy(observation)
+            finished = False
+            episode_length = 0
+            cumulative_reward = 0
 
-            cumulative_reward += reward
-            next_observation = torch.from_numpy(next_observation)
-    
-            action = np.array([action], dtype=np.float32)
-            action = torch.from_numpy(action)
+            while not finished:
+                if episode < configs['warm_up_episodes']:
+                    action = env.action_space.sample()
+                else:
+                    action = q_net(observation.unsqueeze(0)).argmax(axis=-1).squeeze().item()
 
-            buffer.add(observation, action, next_observation, reward, terminated)
+                next_observation, reward, terminated, truncated, _ = env.step(action)
 
-            observation = next_observation
-            finished = terminated or truncated
-            episode_length += 1
+                cumulative_reward += reward
+                next_observation = torch.from_numpy(next_observation)
+        
+                action = np.array([action], dtype=np.float32)
+                action = torch.from_numpy(action)
 
-        print("Episode: ", episode, "Cumulative Reward:", cumulative_reward, "Episode length:", episode_length)
+                buffer.add(observation, action, next_observation, reward, terminated)
 
+                observation = next_observation
+                finished = terminated or truncated
+                episode_length += 1
 
-        if len(buffer) >= configs['batch_size']:
-            for _ in range(configs['train_steps']):
-                _ = learn(
-                    q_net,
-                    target_q_net,
-                    buffer.sample(configs['batch_size']),
-                    optimizer,
-                    configs['gamma'],
-                )
+            print("Episode: ", episode, "Cumulative Reward:", cumulative_reward, "Episode length:", episode_length)
+            writer.writerow({'episode': episode, 'reward': cumulative_reward, 'episode_length': episode_length})
 
-            if episode % configs['target_update_freq'] == 0: # delayed hard update
-                target_q_net.load_state_dict(q_net.state_dict())
+            if len(buffer) >= configs['batch_size']:
+                for _ in range(configs['train_steps']):
+                    _ = learn(
+                        q_net,
+                        target_q_net,
+                        buffer.sample(configs['batch_size']),
+                        optimizer,
+                        configs['gamma'],
+                    )
+
+                if episode % configs['target_update_freq'] == 0:
+                    target_q_net.load_state_dict(q_net.state_dict())
 
 
 if __name__ == '__main__':
